38830564c38d09042de4a1dba9d816a4ee0079f8
Mark Ranallo <mark@planningcenter.com>
2025-03-06 16:39:46 -0800

    Add path to GCC for native compilation.

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; package --- Summary
+;;; Commentary:
+;;; I am not sure how this works, but it is the first init file read.
+;;; Code:
+;; (add-to-list 'default-frame-alist '(undecorated-round . t))
+(provide 'early-init)
+;;; early-init.el ends here
